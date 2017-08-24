f9bf892ba433401b06557038fdaa50019649eaec
Marty Bonacci <martybonacci@gmail.com>
2017-08-24 11:38:35 -0600

    woot! woot!

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,18 @@
 import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./components/home.component";
+import {AboutComponent} from "./components/about.component";
 
-export const allAppComponents = [HomeComponent];
+import {NavbarComponent} from "./components/navbar.component";
+import {PostsComponent} from "./components/posts.component";
+import {FooterComponent} from "./components/footer.component";
+
+export const allAppComponents = [HomeComponent, AboutComponent, NavbarComponent, PostsComponent,FooterComponent];
 
 export const routes: Routes = [
+	{path: "about",component:AboutComponent},
+	{path: "footer",component:FooterComponent},
+	{path: "navbar",component:NavbarComponent},
+	{path: "posts",component:PostsComponent},
 	{path: "", component: HomeComponent},
 	{path: "**", redirectTo: ""}
 ];
